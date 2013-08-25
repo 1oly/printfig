c62f05556dd05dd381ed8e0d8c8fcab32d72e50c
Oliver <oliver@dtu>
2013-08-25 13:54:40 +0200

    Added polar option
    Better outcome between matlab2tikz and mmpolar

--- printfig.m
+++ printfig.m
@@ -1,18 +1,26 @@
-function printfig(dest,h,w,compile)
+function printfig(dest,h,w,compile,polar)
 % Use matlab2tikz to compile a .tex tikz-figure and optionally show pdf.
 %
+% Usage: printfig([figpath '/fig_name.tex'],'height','width',1,0)
 % Input:
 %	dest: String specifying destination and name of figure, e.g 'myfigure.tex'
 %	h: String with height of figure, e.g. '4cm'
 %	w: String with width of figure, e.g. '4cm'
 %	compile: Logical (1 or 0) option to compile with pdflatex
-%
+%   polar: Logical (1 or 0) option to get better polar plots
 %	Author: Oliver Lylloff
 %	Date: 24/5/13
 %	
+if nargin == 4
+    polar = false;
+elseif nargin == 5 && polar == 1
+    polar = true;
+end
 matlab2tikz(dest,'height', h, 'width',w,...
 'parseStrings',false,...
-'extraAxisOptions', {...		% Begin custom axis options
+'colormap',colormap(fireprint),...
+'showHiddenStrings',polar,...
+'extraAxisOptions', {...
 'scaled x ticks = false',...
 '/pgf/number format/set thousands separator={}'...
 },'standalone',true);
